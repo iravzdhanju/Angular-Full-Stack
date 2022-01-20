cb6e21de15c9352d471daee8b700489f57612388
Ravinderpal Singh <ravzdhanju@gmail.com>
2022-01-20 17:43:47 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { UserComponent } from './component/user/user.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
+    , UserComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/component/user/user.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core'
+@Component({
+    selector: 'app-user',
+    templateUrl: './user.component.html',
+    styleUrls: ['./user.component.scss'],
+
+
+})
+
+export class UserComponent {
+    firstName: string = 'John';
+
+    lastName: string = 'Doe';
+    age: number = 20;
+
+}
